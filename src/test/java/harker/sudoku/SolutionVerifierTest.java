fd7f1ce2b70820386c17ae88cc65d01e3d6faedf
Iaroslav Mazai <harker6666@gmail.com>
2015-05-05 21:46:25 +0300

    SolutionVerifier unhappy path with invalid column test created

--- src/test/java/harker/sudoku/SolutionVerifierTest.java
+++ src/test/java/harker/sudoku/SolutionVerifierTest.java
@@ -20,6 +20,7 @@ public class SolutionVerifierTest {
 			{0, 0, 0, 0, 0, 0, 0, 0, 9},
 	};
 
+	// Second line has duplicating 5
 	private int[][] solutionWithInvalidLine = {
 			{1, 0, 0, 0, 0, 0, 0, 0, 0},
 			{5, 2, 0, 5, 8, 0, 9, 7, 0},
@@ -32,6 +33,19 @@ public class SolutionVerifierTest {
 			{0, 0, 0, 0, 0, 0, 0, 0, 9},
 	};
 
+	// Third column has duplicating 2
+	private int[][] solutionWithInvalidColumn = {
+			{1, 0, 0, 0, 0, 0, 0, 0, 0},
+			{5, 2, 0, 0, 8, 0, 9, 7, 0},
+			{0, 4, 3, 7, 0, 0, 0, 0, 0},
+			{0, 0, 0, 4, 0, 0, 0, 0, 0},
+			{0, 1, 2, 0, 5, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 6, 0, 0, 0},
+			{0, 0, 2, 0, 0, 0, 7, 0, 0},
+			{0, 6, 7, 0, 0, 0, 0, 8, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 9},
+	};
+
 	@Test
 	public void shouldSolutionVerifierReturnTrueOnValidSolutionPassed() {
 		assertTrue(testInstance.verifySolution(validSolution));
@@ -40,5 +54,10 @@ public class SolutionVerifierTest {
 	@Test
 	public void shouldSolutionVerifierReturnFalseOnSolutionWithInvalidLinePassed() {
 		assertFalse(testInstance.verifySolution(solutionWithInvalidLine));
+	}
+
+	@Test
+	public void shouldSolutionVerifierReturnFalseOnSolutionWithInvalidColumnPassed() {
+		assertFalse(testInstance.verifySolution(solutionWithInvalidColumn));
 	}
 }
