dab88e7d4934996e51d60c9888251ff94e09132a
Iaroslav Mazai <harker6666@gmail.com>
2015-05-05 21:41:05 +0300

    SolutionVerifier unhappy path with invalid line implemented

--- src/main/java/harker/sudoku/SolutionVerifier.java
+++ src/main/java/harker/sudoku/SolutionVerifier.java
@@ -1,8 +1,31 @@
 package harker.sudoku;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SolutionVerifier {
 
 	public boolean verifySolution(int[][] solution) {
+		for (int[] line : solution) {
+			if (hasRepeats(line)) {
+				return false;
+			}
+		}
+
 		return true;
+	}
+
+	private boolean hasRepeats(int[] line) {
+		Set<Integer> encounteredInts = new HashSet<>();
+		for (int cell : line) {
+			if (cell != 0) {
+				if (encounteredInts.contains(cell)) {
+					return true;
+				} else {
+					encounteredInts.add(cell);
+				}
+			}
+		}
+		return false;
 	}
 }
